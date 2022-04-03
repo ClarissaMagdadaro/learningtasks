c3ad946bb825ceeae5456a87c8a9daf0b6b07e2e
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-04-03 17:31:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <ul>
        <li><a href="index.php"> HOME</a></li>
        <li><a href="/LT2/Recipes.html">LEARNING TASK 2</a></li>
-       <li><a href="/LT3/LE3.html">LEARNING TASK 3</a></li>
+       <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
        <li><a href="/CLE1MT/CLE1MT.html">CLE1MT</a></li>
         </ul>
     </body>
