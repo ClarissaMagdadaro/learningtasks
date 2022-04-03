ad99f2a8d0d07e20027600d6ad73383040027010
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-04-03 17:12:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <h1> Welcome to My Website! I am Clarissa Bianca B. Magdadaro :> </h1>
     <ul>
        <li><a href="index.html"> HOME</a></li>
-       <li><a href="Recipes.html">LEARNING TASK 2</a></li>
-       <li><a href="LE3.html">LEARNING TASK 3</a></li>
-       <li><a href="CLE1MT.html">CLE1MT</a></li>
+       <li><a href="/LT2">LEARNING TASK 2</a></li>
+       <li><a href="/L3">LEARNING TASK 3</a></li>
+       <li><a href="/CLE1MT">CLE1MT</a></li>
         </ul>
     </body>
