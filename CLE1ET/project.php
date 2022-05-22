d189f19928322afd29dee5a7ff9425a9b6b81a95
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-05-22 20:40:45 +0800

    Add files via upload

---
+++ CLE1ET/project.php
@@ -0,0 +1,50 @@
+<div id="project">
+<div id="clarissa"> <h1 id="cbm"> PROJECTS </h1></div>
+    <div id="divide2"> 
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT2</h1>
+                <div id="view"><h1><a href="https://clarissamagdadaro.herokuapp.com/LT2/Recipes.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
+            </div>
+            <img alt="pic" src="LT2.png"></div>
+        
+        
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT3</h1>
+                <div id="view"><h1><a href="https://clarissamagdadaro.herokuapp.com/LT3/index.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
+            </div>
+            <img alt="pic" src="LT3.png">
+            
+            
+        </div>
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT4</h1>
+                <div id="view"><h1><a href="https://clarissamagdadaro.herokuapp.com/LT4/index.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
+            </div>
+            <img alt="pic" src="LT4.png">
+            
+            
+        </div>
+        <div id="boxB"> </div>
+        <div id="boxA">
+            <div id="namebox">
+                <h1>CLE1MT</h1>
+                <div id="view"><h1><a href="https://clarissamagdadaro.herokuapp.com/CLE1MT/CLE1MT.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
+            </div>
+            <img alt="pic" src="CLE1MT.png">
+            
+            
+        </div>
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT5</h1>
+                <div id="view"><h1><a href="https://github.com/ClarissaMagdadaro/learningtasks/tree/main/LT5" target="_blank" rel="noopener noreferrer">view</a></h1></div>
+            </div>
+            <img alt="pic" src="LT5.png">
+        </div>
+        
+    </div>
+    
+</div>
