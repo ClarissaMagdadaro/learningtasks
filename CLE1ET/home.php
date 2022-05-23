ce140653cb9c7f9b162fdbce706b21834f64df8f
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-05-23 10:04:27 +0800

    Delete CLE1ET directory

--- CLE1ET/home.php
+++
@@ -1,6 +0,0 @@
-<div id= "home"> 
-    <div id= "name">
-    <h1>HELLO! I AM<br> CLARISSA MAGDADARO</h1>
-    <h2>Want to know me better? click the button below to learn more about me!</h2>
-    <h1><a href="about-me.php"> ABOUT ME </a></h1></div>
-</div>
