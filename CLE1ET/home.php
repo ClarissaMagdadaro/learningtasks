5ad606748f7353dc18312177eecf7c7797010e6b
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-05-23 10:20:50 +0800

    Add files via upload

---
+++ CLE1ET/home.php
@@ -0,0 +1,6 @@
+<div id= "home"> 
+    <div id= "name">
+    <h1>HELLO! I AM CLARISSA MAGDADARO</h1>
+    <h2>Welcome to my Homepage!<h2>
+    <h1><a href="about-me.php"> ABOUT ME </a></h1></div>
+</div>
