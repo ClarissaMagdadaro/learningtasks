e7a46b95818f32fef40f6e4d15519c1f5519403a
ClarissaMagdadaro <99953732+ClarissaMagdadaro@users.noreply.github.com>
2022-05-23 13:02:53 +0800

    Add files via upload

---
+++ CLE1ET/LT5/js/script.js
@@ -0,0 +1,5 @@
+/*eslint-env browser*/
+function Calculator() {
+
+    document.getElementById('.box').calcy.answers.value += '+';
+}
---
+++ CLE1ET/profile.php
@@ -0,0 +1,9 @@
+<div id="profile">
+    <div id="divide">
+        <img src="profpic.jpg" width="400" height="500">
+        <div id="box"> 
+        <p>I pursued Information Technology because it piqued my attention and my curiosity. I'm interested in this field because I want to improve my capabilities and one of my family member has pushed me to take this course. I'm also intrigued because I'm interested in knowing more about this course. I hope you'll find my portfolio satisfying. Thank You! </p>
+        </div>
+    </div>
+    
+</div>
